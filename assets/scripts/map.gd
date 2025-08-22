7bcf9a95f19c2be5a3c73bfbd4d84b51ecd50207
ItsGetWreckt <samanyu.rayala@gmail.com>
2025-08-21 19:31:52 -0700

    Fixed some bugs with the shop
    Fixed some bugs with the shop

--- assets/scripts/map.gd
+++ assets/scripts/map.gd
@@ -2,6 +2,8 @@ extends Node2D
 
 @onready var clicks_counter = $CanvasLayer/Label
 @onready var player = $CharacterBody2D
+@onready var label_3 = $Labels/Label3
+@onready var label_5 = $Labels/Label5
 
 var clicks = 0
 var clicker_helpers = 0
@@ -19,6 +21,8 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	clicks_counter.text = "Clicks: " + str(round(clicks))
+	label_3.text = "Clicker \n Helper \n (B) to buy \n Price: " + str(clicker_price)
+	label_5.text = "Click \n Multiplier \n (E) to buy \n Price: " + str(clicks_multiplier_price)
 	clicks += 0.0001 * clicker_helpers
 	if player.position.x > 4000:
 		player.position.x = 216
@@ -31,13 +35,14 @@ func _physics_process(delta):
 	player.jump_velocity = -250 - 0.25 * jump_boosts
 	# Handle sprint. 
 	if Input.is_action_pressed("Sprint"):
-		player.speed = (50.0 + 0.1 * speed_boosts) * 2
+		player.speed = (50.0 + 0.1 * speed_boosts) * 10
 	else:
 		player.speed = 50.0 + 0.1 * speed_boosts
 	
 	if player.position.x >= 2972 and player.position.x <= 3120:
 		if Input.is_action_just_pressed("Shop1") and clicks >= clicker_price:
 			clicker_helpers += 1
+			clicks -= clicker_price
 			clicker_price += 10
 		if Input.is_action_just_pressed("Shop2") and clicks >= 10:
 			speed_boosts += 1
@@ -45,6 +50,7 @@ func _physics_process(delta):
 		if Input.is_action_just_pressed("Shop3") and clicks >= clicks_multiplier_price:
 			clicks_multipliers += 1;
 			clicks -= clicks_multiplier_price
+			clicks_multiplier_price += 15
 		if Input.is_action_just_pressed("Shop4") and clicks >= 10:
 			jump_boosts += 1;
 			clicks -= 10;
